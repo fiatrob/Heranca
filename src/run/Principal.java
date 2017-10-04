51420c9e9032d81c32d68bd8cb74bad872f04bed
fiatrob <track.almeida@gmail.com>
2017-10-03 22:03:01 -0300

    aaa
    aaa

--- src/run/Principal.java
+++ src/run/Principal.java
@@ -5,6 +5,8 @@
  */
 package run;
 import classes.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 //import classes.Inspetor;
 //import classes.Secretaria;
@@ -14,10 +16,11 @@ import java.util.Date;
  * @author robso
  */
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         
         //Criar objetos para interagir com os atributos
-        
+        String data = "24/05/1990";
+        SimpleDateFormat formataData = new SimpleDateFormat("dd/MM/yyyy");
         Aluno aluno = new Aluno();
         Inspetor inspeto = new Inspetor();
         Secretaria secretaria = new Secretaria();
@@ -25,12 +28,13 @@ public class Principal {
         
         aluno.nome = "Robson";
         aluno.cpf = "00000000";
-        aluno.dataNascimento = new Date();
+        aluno.dataNascimento = (Date) formataData.parse(data);
         aluno.matricula = "123456789";
         aluno.webAluno = true;
         aluno.mensalidade = 1234.0;
-        System.out.println(aluno);
-        
+        System.out.println(aluno.nome);
+         System.out.println(aluno.cpf);
+         System.out.println(aluno.dataNascimento);
         
     }
 }
